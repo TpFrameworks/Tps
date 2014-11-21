64196631aeddb22b7db3f8f116ebe355b3bdf8fb
TpFrameworks <insat@192.168.1.6>
2014-11-21 17:15:18 +0100

    modification

---
+++ tp1Test/src/main/java/tp1/tp1Test/App.java
@@ -0,0 +1,20 @@
+package tp1.tp1Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	/**
+	 * 
+	 * methode main 
+	 * 
+	 * @param args tab[String]
+	 */
+    public static void main( String[] args )
+    
+    {
+        System.out.println( "Hello World!" );
+    }
+}
